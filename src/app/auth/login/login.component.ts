c0128fab71d9257f3f11bf19334ae1a630fbeaf9
jihenJN <jihenjihen23@gmail.com>
2023-03-29 01:45:25 +0100

    register and cleaning code is ok

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators ,FormGroup} from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
 import { TokenStorageService } from 'src/app/_services/token-storage.service';
@@ -9,97 +9,70 @@ import { TokenStorageService } from 'src/app/_services/token-storage.service';
   styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent implements OnInit{
- /* formRegister:any={
-    username:null,
-    email:null,
-    password:null
-};
-*/
-  /*isSuccessful = false;
-  isSignUpFailed = false;*/
-  errorMessage = '';
+export class LoginComponent implements OnInit {
+
 
   formLogin: any = {
     username: null,
     password: null
   };
+
   isLoggedIn = false;
   isLoginFailed = false;
+  errorMessage = '';
   errorMessage2 = '';
-  roles: string[] = []; 
-
-  
-
-constructor(private router: Router,
-private authService: AuthService,
-private formBuilder: FormBuilder, 
-private tokenStorage: TokenStorageService) { }
-
-ngOnInit(): void {
-/*this.formRegister = this.formBuilder.group({
-    username: ['',Validators.required],
-    email: ['',Validators.required],
-    password: ['',Validators.required],
-   
-  });*/
-
-  this.formLogin = this.formBuilder.group({
-    username: ['', Validators.required],
-    password: ['', Validators.required]
-});
+  roles: string[] = [];
 
 
-  if (this.tokenStorage.getToken()) {
-    this.isLoggedIn = true;
-    this.roles = this.tokenStorage.getUser().roles;
-  } 
-}
-/*
-onSubmitRegister(): void {
-const { username, email, password } = this.formRegister;
-console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-this.authService.register(username, email, password).subscribe({
-  next: (data: any) => {
-    console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-    console.log(data);
-    this.isSuccessful = true;
-    this.isSignUpFailed = false;
-  },
-  error: (err: { error: { message: any; }; }) => {
-    this.errorMessage = err.error.message;
-    this.isSignUpFailed = true;
+
+  constructor(private router: Router,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private tokenStorage: TokenStorageService) { }
+
+  ngOnInit(): void {
+
+
+    this.formLogin = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
   }
-});
-}*/
 
-onSubmitLogin(): void {
-const { username, password } = this.formLogin;
-console.log("test");
 
-this.authService.login(username, password).subscribe({
-  next: data => {
-    console.log("data : ", data); 
-    this.tokenStorage.saveToken(data.accessToken);
-    this.tokenStorage.saveUser(data);
+  onSubmitLogin(): void {
+    const { username, password } = this.formLogin;
+    console.log("test");
 
-    this.isLoginFailed = false;
-    this.isLoggedIn = true;
-    this.roles = this.tokenStorage.getUser().roles;
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        console.log("data : ", data);
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
 
-    console.log("Roles : ", this.roles);   
-    console.log("user : ", this.tokenStorage.getUser()) ;    
-    this.router.navigate(['product/home']);
-  },
-  error: err => {
-    this.errorMessage = err.error.message;
-    this.isLoginFailed = true;
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getUser().roles;
+
+        console.log("Roles : ", this.roles);
+        console.log("user : ", this.tokenStorage.getUser());
+        this.router.navigate(['product/home']);
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
   }
-});
-}
 
-reloadPage(): void {
-window.location.reload();
-}
+  reloadPage(): void {
+    window.location.reload();
+  }
 
 }
